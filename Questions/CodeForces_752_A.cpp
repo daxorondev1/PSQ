ca31e0adb7c45a33e7575ef81a3624b6cd2d644a
daxoron <daxoron.dev@gmail.com>
2021-11-21 08:52:40 +0530

    Modification Template.cpp

---
+++ Questions/CodeForces_752_A.cpp
@@ -0,0 +1,53 @@
+/*
+
+  |
+,-| ,-. . , ,-. ,-. ,-. ,-.    
+| | ,-|  X  | | |   | | | |    
+`-' `-^ ' ` `-' '   `-' ' ' 
+
+
+
+    | |                                 
+  __| | __ ___  _____  _ __ ___  _ __   
+ / _` |/ _` \ \/ / _ \| '__/ _ \| '_ \  
+| (_| | (_| |>  < (_) | | | (_) | | | | 
+ \__,_|\__,_/_/\_\___/|_|  \___/|_| |_| 
+
+
+
+  _____     ____      __ __    _____    _____     _____    ____   _    
+ |     \   |    \     \ ` /   /     \  |     |   /     \  |    \ | |   
+ |      \  |     \    /   \   |     |  |     \   |     |  |     \| |   
+ |______/  |__|\__\  /__/\_\  \_____/  |__|\__\  \_____/  |__/\____|
+
+ 
+*/
+#include <bits/stdc++.h>
+#define test long long int t; cin >> t; while (t--)
+using namespace std;
+int main(){
+  //InputOutput
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  //Testcases
+  test
+  {
+      long long int n,i;
+      cin>>n;
+      long long int arr[n];
+      for(i = 0; i<n; i++)
+      {
+          cin>>arr[i];
+      }
+
+      long long int temp,res=0,temp1=0,index_temp=0;
+      for(i = 0; i<n; i++)
+      {
+           temp = arr[i] - (i+1);
+           if(res<temp)
+                res = temp;
+      }
+      cout<<res<<"\n";
+  }
+  return 0;
+}
--- Questions/CodeForces_752_B.cpp
+++ Questions/CodeForces_752_B.cpp
@@ -1,29 +1,38 @@
 /*
+
+  |
+,-| ,-. . , ,-. ,-. ,-. ,-.    
+| | ,-|  X  | | |   | | | |    
+`-' `-^ ' ` `-' '   `-' ' ' 
+
+
+
+    | |                                 
+  __| | __ ___  _____  _ __ ___  _ __   
+ / _` |/ _` \ \/ / _ \| '__/ _ \| '_ \  
+| (_| | (_| |>  < (_) | | | (_) | | | | 
+ \__,_|\__,_/_/\_\___/|_|  \___/|_| |_| 
+
+
+
   _____     ____      __ __    _____    _____     _____    ____   _    
  |     \   |    \     \ ` /   /     \  |     |   /     \  |    \ | |   
  |      \  |     \    /   \   |     |  |     \   |     |  |     \| |   
  |______/  |__|\__\  /__/\_\  \_____/  |__|\__\  \_____/  |__/\____|
+
+ 
 */
 #include <bits/stdc++.h>
 #define test long long int t; cin >> t; while (t--)
 using namespace std;
-
-void solve()
-{
-  //Place Your Logic Here :DAXORON:
-}
-
 int main(){
-  //ForLocalIO
-  #ifndef ONLINE_JUDGE
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-  #endif
   //InputOutput
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-
   //Testcases
-  test solve();
+  test
+  {
+      
+  }
   return 0;
 }
